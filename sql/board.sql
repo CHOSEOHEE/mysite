6002a8351885e30a948ab6687a889ff57ca2fe13
CHOSEOHEE <whtjgml2002@naver.com>
2025-08-29 13:05:06 +0900

    board finish

--- sql/board.sql
+++ sql/board.sql
@@ -1,5 +1,37 @@
 --
+-- user
+--
+
+desc user;
+
+-- findAll
+SELECT id FROM user WHERE id = 1;
+
+-- 회원리스트
+ select * from user;
+
+-- 회원가입
+insert into user(name, email, password, gender, join_date) values ('둘리', 'dooly@gmail.com', password('1234'), 'male', current_date());
+
+
+--
 -- board
 --
 
 desc board;
+alter table board modify id int not null auto_increment;
+
+-- findAll
+ select * from board order by id desc;
+ALTER TABLE board 
+MODIFY reg_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE board
+MODIFY COLUMN g_no INT NOT NULL;
+
+-- deleteByIdDepthGroupAndUser
+delete from board where id = 3 and depth = 0 and g_no =0 and user_id = 1;
+
+-- insert
+INSERT INTO board (title, contents, hit, g_no, o_no, depth, user_id)
+VALUES ('제목', '내용', 0, 1, 1, 0, 1);
+
